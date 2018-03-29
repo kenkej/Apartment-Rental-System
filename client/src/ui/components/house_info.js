9a95a29cd70b0904a95f0f0e8eac39dabb5d2d17
Thai Hieu Trung <thtrung09031993@gmail.com>
2018-03-29 13:52:03 +0700

    add deposit and rent status

--- client/src/ui/components/house_info.js
+++ client/src/ui/components/house_info.js
@@ -1,20 +1,29 @@
 import React from 'react'
 
 class HouseInfo extends React.Component {
+
+    handleRentEvent(event) {
+
+    }
+
     render() {
-        return(
+        return (
             <div className="house">
-                <div className="card" style={{width: 18+'rem'}}>
-                    <img className="card-img-top" src={this.props.img_src}/>
+                <div className="card" style={{ width: 18 + 'rem' }}>
+                    <img className="card-img-top" src={this.props.img_src} />
                     <div className="card-body">
                         <h5 className="card-title">{this.props.house_name}</h5>
                         <p className="card-text">{this.props.house_description}</p>
                         <h6 className="card-title">{this.props.house_price} ETH per day</h6>
-                        <h6 className="card-title">{this.props.house_deposit} ETH deposit</h6>                        
-                        {this.props.house_available ? 
-                        <h6 style={{color : 'green'}}>AVAILABLE</h6> : 
-                        <h6 style={{color : 'red'}}>RENTED</h6>}
+                        <h6 className="card-title">{this.props.house_deposit} ETH deposit</h6>
+                        {this.props.house_available ?
+                            <h6 style={{ color: 'green' }}>AVAILABLE</h6> :
+                            <h6 style={{ color: 'red' }}>RENTED</h6>}
                     </div>
+                    {this.props.house_available ?
+                        <button onClick={this.handleRentEvent.bind(this)} className="bg-primary buttonRent">RENT THIS HOUSE</button> :
+                        <div></div>
+                    }
                 </div>
             </div>
         )
